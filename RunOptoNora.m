a4f73f7fbd1e3d83f5e6eb1322b9587b6f7d206e
JorritMontijn <jorritmontijn@gmail.com>
2020-08-28 14:14:24 +0200

    Update RunOptoNora.m

--- RunOptoNora.m
+++ RunOptoNora.m
@@ -8,36 +8,41 @@ close all;
 %% define variables
 intStimSet = 1;% 1=0:15:359, reps20; 2=[0 5 90 95], reps 400 with noise
 boolUseSGL = false;
-boolUseDaq = true;
+intUseDaqDevice = 0; %set to 0 to skip I/O
 intDebug = 0;
+boolDaqIn = false;
 
 %% set parameters
 %
 % 1) one "test" protocol, where we can test a) whether there is a response, b) which light intensity we need.
 %I would make the pulse 5ms long, but maybe its good to also have longer pulses to be flexible (10ms, 20ms, 100ms, 500ms).
-%vecPulseDur = 
+%vecPulseDur =
 
 if intStimSet == 1
-%2) Pulse trains (as discussed)
-%--> 5 pulses, each 5ms long, varying intervals between the pulses according to the frequency
-%Frequencies: 1Hz, 2Hz, 5Hz, 10Hz, 20Hz, 50Hz, 100Hz
-%After each pulse train 10s pause
-%Can run up to 500 trials (as discussed it should repeat the frequencies in a row and then start at 1Hz again) or until stopped
-
-dblPrePostWait = 60;%secs
-dblSamplingRate = 10000;%Hz
-intRepsPerPulse = 5;%count
-intTrialNum = 500;%count
-dblPulseWait = 2;%secs
-vecPulseITI = 1./[1 2 5 10 20 50 100];%secs
-dblPulseDur = 5/1000;%secs
-vecPulseDur = dblPulseDur*ones(size(vecPulseITI));%secs
+	%2) Pulse trains (as discussed)
+	%--> 5 pulses, each 5ms long, varying intervals between the pulses according to the frequency
+	%Frequencies: 1Hz, 2Hz, 5Hz, 10Hz, 20Hz, 50Hz, 100Hz
+	%After each pulse train 10s pause
+	%Can run up to 500 trials (as discussed it should repeat the frequencies in a row and then start at 1Hz again) or until stopped
+	
+	dblPrePostWait = 2;%secs
+	dblSamplingRate = 10000;%Hz
+	intRepsPerPulse = 5;%count
+	intTrialNum = 500;%count
+	dblPulseWait = 2;%secs, at least ~0.2s
+	vecPulseITI = 1./[1 2 5 10 20 50 100];%secs
+	dblPulseDur = 5/1000;%secs
+	vecPulseDur = dblPulseDur*ones(size(vecPulseITI));%secs
+	dblPulseWaitSignal = dblPulseWait/2;
+	dblPulseWaitPause = dblPulseWait - dblPulseWaitSignal;
 end
 
-%% generate data
+%% generate data & pre-allocate
 cellPulseData = cell(1,intTrialNum);
 cellPulseITI = cell(1,intTrialNum);
 cellPulseDur = cell(1,intTrialNum);
+vecStimOnNI = nan(1,intTrialNum);
+vecStimOffNI = nan(1,intTrialNum);
 for intTrial=1:intTrialNum
 	%shuffle order
 	vecRand = randperm(numel(vecPulseITI));
@@ -45,15 +50,15 @@ for intTrial=1:intTrialNum
 	vecShuffDur = vecPulseDur(vecRand);
 	vecData = uint8([]);
 	
-	for intPulseType=1:numel(vecShuffITI) 
+	for intPulseType=1:numel(vecShuffITI)
 		vecOnePulse = uint8(cat(1,ones(round(vecShuffDur(intPulseType)*dblSamplingRate),1),zeros(round(vecShuffITI(intPulseType)*dblSamplingRate),1)));
 		vecPulses = repmat(vecOnePulse,[intRepsPerPulse 1]);
-		vecWait = uint8(zeros(round(dblSamplingRate*dblPulseWait),1));
+		vecWait = uint8(zeros(round(dblSamplingRate*dblPulseWaitSignal),1));
 		vecData = cat(1,vecData,vecPulses,vecWait);
 	end
 	cellPulseData{intTrial} = vecData;
 	cellPulseITI{intTrial} = vecShuffITI;
-cellPulseDur{intTrial} = vecShuffDur;
+	cellPulseDur{intTrial} = vecShuffDur;
 end
 %% define paths
 strThisPath = mfilename('fullpath');
@@ -96,8 +101,6 @@ try
 	intOffset = length(mfilename) + 2;
 	strDir = strThisDir(1:end-intOffset);
 	fprintf('Saving output in directory %s; \n',strSessionDir);
-	strOldPath = cd(strTexDir);
-	cd(strOldPath);
 	if isa(strFilename,'char') && ~isempty(strFilename)
 		%make directory
 		strOutputDir = strcat(strSessionDir,filesep,strRecording,filesep); %where are the logs saved?
@@ -119,9 +122,6 @@ fprintf('Preparing variables...\n');
 %general variable definitions
 structEP = struct; %structureElectroPhysiology
 
-%get default settings in Set
-intUseDaqDevice = boolUseDaq; %set to 0 to skip I/O
-
 %assign filename
 structEP.strFile = mfilename;
 
@@ -134,21 +134,137 @@ sStimParamsSettings = struct;
 sStimParamsSettings.strStimType = 'OptoNora';
 
 %% initialize NI I/O box
+if intUseDaqDevice > 0
+	objDAQOut = openDaqOutputOptoSpritzer(intUseDaqDevice,dblSamplingRate);
+else
+	objDAQOut = struct;
+end
+
+%% assign to structure
+structEP.strRecording = strRecording;
+structEP.strFilename = strFilename;
+structEP.dblPrePostWait = dblPrePostWait;%secs
+structEP.dblSamplingRate = dblSamplingRate;%Hz
+structEP.intRepsPerPulse = intRepsPerPulse;%count
+structEP.intTrialNum = intTrialNum;%count
+structEP.dblPulseWait = dblPulseWait;%secs, at least ~0.2s
+structEP.vecPulseITI = vecPulseITI;%secs
+structEP.dblPulseDur = dblPulseDur;%secs
+structEP.vecPulseDur = vecPulseDur;%secs
+structEP.dblPulseWaitSignal = dblPulseWaitSignal;
+structEP.dblPulseWaitPause = dblPulseWaitPause;
+
+structEP.sStimParamsSettings = sStimParamsSettings;
+structEP.sParamsSGL = sParamsSGL;
+structEP.objDAQOut = objDAQOut;
+
+%save parameters
+save([strOutputDir filesep strFilename], 'structEP');
 
 try
+	%% check escape
+	if CheckEsc(),error([mfilename ':EscapePressed'],'Esc pressed; exiting');end
+	
 	%% start pre-wait
+	hTicExpStart = tic;
+	fprintf('Experiment started; initial wait of %.1fs [%s]\n',dblPrePostWait,getTime);
+	while toc(hTicExpStart) < dblPrePostWait
+		if CheckEsc(),error([mfilename ':EscapePressed'],'Esc pressed; exiting');end
+		pause(1/1000);
+	end
 	
 	%% run stimuli
-	%check for escape in-between pulse runs
+	for intTrial = 1:intTrialNum
+		%timestamp
+		hTicTrial = tic;
+		
+		%save current data
+		cellPulseData_Temp = cellPulseData(1:(intTrial-1));
+		cellPulseITI_Temp = cellPulseITI(1:(intTrial-1));
+		cellPulseDur_Temp = cellPulseDur(1:(intTrial-1));
+		vecStimOnNI_Temp = vecStimOnNI(1:(intTrial-1));
+		vecStimOffNI_Temp = vecStimOffNI(1:(intTrial-1));
+		save([strOutputDir filesep strFilename '_Temp'],...
+			'cellPulseData_Temp','cellPulseITI_Temp','cellPulseDur_Temp','vecStimOnNI_Temp','vecStimOffNI_Temp');
+		
+		%get new pulse data
+		matData = cellPulseData{intTrial};
+		
+		%msg
+		fprintf('Trial %d/%d [%s]\n',intTrial,intTrialNum,getTime);
+		
+		%check for escape in-between pulse runs
+		if CheckEsc(),error([mfilename ':EscapePressed'],'Esc pressed; exiting');end
+		
+		%prep stimulus
+		if intUseDaqDevice > 0
+			stop(objDAQOut);
+			%extend
+			if size(matData,2) == 1
+				matData = repmat(matData,[1 2]);
+			end
+			%prep
+			stop(objDAQOut);
+			queueOutputData(objDAQOut,matData);
+			prepare(objDAQOut);
+		end
+		
+		%wait
+		while toc(hTicTrial) < (dblPulseWaitPause*0.9)
+			pause((dblPulseWaitPause - toc(hTicTrial))*0.3);
+		end
+		while toc(hTicTrial) < dblPulseWaitPause
+			%do nothing
+		end
+		
+		%start stimulus
+		fprintf('\b; stim started at %.3fs\n',toc(hTicTrial));
+		if intUseDaqDevice > 0,startBackground(objDAQOut);end
+		
+		%log NI timestamp
+		if boolUseSGL
+			dblStimOnNI = GetScanCount(hSGL, intStreamNI)/dblSampFreqNI;
+		else
+			dblStimOnNI = nan;
+		end
+		
+		%wait
+		if intUseDaqDevice > 0
+			wait(objDAQOut);
+		end
+		
+		%log NI timestamp
+		if boolUseSGL
+			dblStimOffNI = GetScanCount(hSGL, intStreamNI)/dblSampFreqNI;
+		else
+			dblStimOffNI = nan;
+		end
+		
+		%log timestamps
+		vecStimOnNI(intTrial) = dblStimOnNI;
+		vecStimOffNI(intTrial) = dblStimOffNI;
+		
+		%msg
+		fprintf('\b; trial finished at %.3fs [%s]\n',toc(hTicTrial),getTime);
+	end
 	
-	%log timestamps & save after each trial
-	%cellPulseData{intTrial} = vecData;
-	%cellPulseITI{intTrial} = vecShuffITI;
-	%cellPulseDur{intTrial} = vecShuffDur;
-	
-	%[strOutputDir filesep strFilename]
+	%save data
+	structEP.cellPulseData = cellPulseData(1:intTrial);
+	structEP.cellPulseITI = cellPulseITI(1:intTrial);
+	structEP.cellPulseDur = cellPulseDur(1:intTrial);
+	structEP.vecStimOnNI = vecStimOnNI(1:intTrial);
+	structEP.vecStimOffNI = vecStimOffNI(1:intTrial);
+	save([strOutputDir filesep strFilename], 'structEP');
 	
 	%% end-wait
+	hTicExpStop = tic;
+	fprintf('Starting final wait of %.1fs [%s]\n',dblPrePostWait,getTime);
+	while toc(hTicExpStart) < dblPrePostWait
+		if CheckEsc(),error([mfilename ':EscapePressed'],'Esc pressed; exiting');end
+		pause(1/1000);
+	end
+	
+	%closing remark
 	fprintf('\nExperiment is finished at [%s], closing down and cleaning up...\n',getTime);
 	
 	%end recording
@@ -166,17 +282,12 @@ catch ME
 	fprintf('\n\n\nError occurred! Trying to save data and clean up...\n\n\n');
 	
 	%save data
-	structEP.sStimParams = sStimParams;
-	structEP.sStimObject = sStimObject;
-	structEP.sStimTypeList = sStimTypeList;
+	structEP.cellPulseData = cellPulseData(1:intTrial);
+	structEP.cellPulseITI = cellPulseITI(1:intTrial);
+	structEP.cellPulseDur = cellPulseDur(1:intTrial);
+	structEP.vecStimOnNI = vecStimOnNI(1:intTrial);
+	structEP.vecStimOffNI = vecStimOffNI(1:intTrial);
 	save([strOutputDir filesep strFilename], 'structEP');
-	
-	%% catch me and throw me
-	Screen('Close');
-	Screen('CloseAll');
-	ShowCursor;
-	Priority(0);
-	Screen('Preference', 'Verbosity',intOldVerbosity);
 	
 	%% end recording
 	try
