27ad0d87f639e47804ab4fd09afcd1ec4f726bf7
JorritMontijn <jorritmontijn@gmail.com>
2020-11-18 16:55:33 +0100

    20201118

--- runPreProcessPix.m
+++ runPreProcessPix.m
@@ -104,6 +104,11 @@ ops.chanMap = fullfile(strPathToConfigFile, chanMapFile);
 %% this block runs all the steps of the algorithm
 fprintf('Looking for data inside %s \n', rootZ)
 
+% main parameter changes from Kilosort2 to v2.5
+ops.sig        = 20;  % spatial smoothness constant for registration
+ops.fshigh     = 300; % high-pass more aggresively
+ops.nblocks    = 5; % blocks for registration. 0 turns it off, 1 does rigid registration. Replaces "datashift" option. 
+
 % is there a channel map file in this folder?
 fs = dir(fullfile(rootZ, 'chan*.mat'));
 if ~isempty(fs)
@@ -116,15 +121,19 @@ ops.fbinary = fullfile(rootZ, fs(1).name);
 
 % preprocess data to create temp_wh.dat
 rez = preprocessDataSub(ops);
+%
+% NEW STEP TO DO DATA REGISTRATION
+rez = datashift2(rez, 1); % last input is for shifting data
 
-% time-reordering as a function of drift
-rez = clusterSingleBatches(rez);
-
-% saving here is a good idea, because the rest can be resumed after loading rez
-save(fullfile(rootZ, 'rez.mat'), 'rez', '-v7.3');
-
+% ORDER OF BATCHES IS NOW RANDOM, controlled by random number generator
+iseed = 1;
+                 
 % main tracking and template matching algorithm
-rez = learnAndSolve8b(rez);
+rez = learnAndSolve8b(rez, iseed);
+
+% OPTIONAL: remove double-counted spikes - solves issue in which individual spikes are assigned to multiple templates.
+% See issue 29: https://github.com/MouseLand/Kilosort/issues/29
+%rez = remove_ks2_duplicate_spikes(rez);
 
 % final merges
 rez = find_merges(rez, 1);
@@ -132,11 +141,10 @@ rez = find_merges(rez, 1);
 % final splits by SVD
 rez = splitAllClusters(rez, 1);
 
-% final splits by amplitudes
-rez = splitAllClusters(rez, 0);
-
 % decide on cutoff
 rez = set_cutoff(rez);
+% eliminate widely spread waveforms (likely noise)
+rez.good = get_good_units(rez);
 
 fprintf('found %d good units \n', sum(rez.good>0))
 
@@ -145,9 +153,23 @@ fprintf('Saving results to Phy  \n')
 rezToPhy(rez, rootZ);
 
 %% if you want to save the results to a Matlab file...
+
 % discard features in final rez file (too slow to save)
 rez.cProj = [];
 rez.cProjPC = [];
+
+% final time sorting of spikes, for apps that use st3 directly
+[~, isort]   = sortrows(rez.st3);
+rez.st3      = rez.st3(isort, :);
+
+% Ensure all GPU arrays are transferred to CPU side before saving to .mat
+rez_fields = fieldnames(rez);
+for i = 1:numel(rez_fields)
+    field_name = rez_fields{i};
+    if(isa(rez.(field_name), 'gpuArray'))
+        rez.(field_name) = gather(rez.(field_name));
+    end
+end
 
 % save final results as rez2
 fprintf('Saving final results in rez2  \n')
