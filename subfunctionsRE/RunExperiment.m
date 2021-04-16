c7c3df1a9d118080033784a9fc2c886cfb33351b
NJamann <n.jamann@nin.knaw.nl>
2021-04-16 18:41:58 +0200

    Create RunExperiment.m

---
+++ subfunctionsRE/RunExperiment.m
@@ -0,0 +1,26 @@
+function RunExperiment
+	% RunExperiment Run Neuropixel Acquisition session
+	
+	%define globals
+	global sFigRE;
+	global sRE;
+	
+	%check if instance is already running
+	if isstruct(sFigRE) && isfield(sFigRE,'IsRunning') && sFigRE.IsRunning == 1
+		error([mfilename ':SingularGUI'],'RunExperiment instance is already running; only one simultaneous GUI is allowed');
+	end
+	
+	%assert preset presence
+	intPresetsCreated = assertPresets();
+	
+	%clear data & disable new instance
+	sFigRE = struct;
+	sRE = struct;
+	sFigRE.IsRunning = true;
+	
+	%generate gui
+	[sFigRE,sRE] = RE_genGUI(sFigRE,sRE);
+	
+end
+
+
