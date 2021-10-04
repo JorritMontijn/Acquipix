b6a6f28ad7edefc119bb3db7795efc6b9923d389
JorritMontijn <jorritmontijn@gmail.com>
2021-10-04 16:25:49 +0200

    Update RP_defaultMetaVar.m

--- subfunctionsRP/RP_defaultMetaVar.m
+++ subfunctionsRP/RP_defaultMetaVar.m
@@ -2,15 +2,14 @@ function sMetaVar = RP_defaultMetaVar()
 	%% default settings
 	sMetaVar = struct;
 	sMetaVar.version = '1.0';
-	sMetaVar.dataset = 'Neuropixels_data';
-	sMetaVar.investigator = 'Jorrit_Montijn';
-	sMetaVar.project = 'MontijnNPX2020';
-	sMetaVar.setup = 'Neuropixels';
+	sMetaVar.dataset = 'Default_data'; %Neuropixels_data
+	sMetaVar.investigator = 'NomenNescio'; %Jorrit_Montijn
+	sMetaVar.project = 'DefaultProject'; %MontijnNPX2020
+	sMetaVar.setup = 'WindowlessBroomCloset';%Neuropixels
 	sMetaVar.stimulus = 'VisStimAcquipix';
 	sMetaVar.condition = 'none';
-	sMetaVar.subjecttype = 'BL6';
+	sMetaVar.subjecttype = 'undefined'; %BL6
 	sMetaVar.niCh0 = 'sync'; %pulse sync channel 
 	sMetaVar.niCh1 = 'onset'; %screen diode channel
-	sMetaVar.niCh2 = '@PP_GetRunSpeed'; %misc channel
-	
+	sMetaVar.niCh2 = '@PP_GetRunSpeed'; %misc channel; e.g., @PP_GetRunSpeed
 end
